69928f7d10877ca38af288a5ecb5160c7dfc6858
suneelkumarsh <33027526+suneelkumarsh@users.noreply.github.com>
2023-09-04 20:19:02 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const Index = () => {
+    return(
+           
+            //using reaact fragment to allow multiline element
+            <>
+              <h1 class="jumbotron text-center bg-primary square">Hello world</h1>
+              <p> from suneel</p>
+            </>
+    );
+
+};
+
+export default Index;
